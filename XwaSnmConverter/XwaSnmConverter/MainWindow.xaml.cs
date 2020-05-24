25a27bbe3c4cb8bb2ab0c8e3847dc7de93178596
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2020-05-24 14:40:57 +0200

    Add support for mp4 files

--- XwaSnmConverter/XwaSnmConverter/MainWindow.xaml.cs
+++ XwaSnmConverter/XwaSnmConverter/MainWindow.xaml.cs
@@ -58,6 +58,126 @@ namespace XwaSnmConverter
             }, dispatcherAction);
         }
 
+        private void smOpenSnm_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.CheckFileExists = true;
+            dialog.Filter = "snm files (*.snm, *.znm)|*.snm;*.znm";
+
+            if (dialog.ShowDialog(this) == true)
+            {
+                this.smSnmFileName.Text = dialog.FileName;
+                this.smMp4FileName.Text = System.IO.Path.ChangeExtension(dialog.FileName, "mp4");
+            }
+        }
+
+        private void smOpenMp4_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.smSnmFileName.Text))
+            {
+                return;
+            }
+
+            var dialog = new SaveFileDialog();
+            dialog.AddExtension = true;
+            dialog.DefaultExt = "mp4";
+            dialog.Filter = "mp4 files (*.mp4)|*.mp4";
+            dialog.InitialDirectory = System.IO.Path.GetDirectoryName(this.smSnmFileName.Text);
+            dialog.FileName = System.IO.Path.ChangeExtension(System.IO.Path.GetFileName(this.smSnmFileName.Text), "mp4");
+
+            if (dialog.ShowDialog(this) == true)
+            {
+                this.smMp4FileName.Text = dialog.FileName;
+            }
+        }
+
+        private void smConvert_Click(object sender, RoutedEventArgs e)
+        {
+            string snm = this.smSnmFileName.Text;
+            string mp4 = this.smMp4FileName.Text;
+
+            if (string.IsNullOrEmpty(snm) || string.IsNullOrEmpty(mp4))
+            {
+                return;
+            }
+
+            this.RunBusyAction(disp =>
+            {
+                try
+                {
+                    JeremyAnsel.Xwa.Snm.SnmFile
+                        .FromFile(snm)
+                        .SaveAsMp4(mp4);
+
+                    disp(() => Xceed.Wpf.Toolkit.MessageBox.Show(this, string.Concat(mp4, " created."), this.Title, MessageBoxButton.OK));
+                }
+                catch (Exception ex)
+                {
+                    disp(() => Xceed.Wpf.Toolkit.MessageBox.Show(this, ex.ToString(), this.Title, MessageBoxButton.OK, MessageBoxImage.Error));
+                }
+            });
+        }
+
+        private void msOpenMf_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.CheckFileExists = true;
+            dialog.Filter = "mp4 files|*.mp4|avi files|*.avi|video files|*.*";
+
+            if (dialog.ShowDialog(this) == true)
+            {
+                this.msMfFileName.Text = dialog.FileName;
+                this.msSnmFileName.Text = System.IO.Path.ChangeExtension(dialog.FileName, "snm");
+            }
+        }
+
+        private void msOpenSnm_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.msMfFileName.Text))
+            {
+                return;
+            }
+
+            var dialog = new SaveFileDialog();
+            dialog.AddExtension = true;
+            dialog.DefaultExt = "snm";
+            dialog.Filter = "snm files (*.snm, *.znm)|*.snm;*.znm";
+            dialog.InitialDirectory = System.IO.Path.GetDirectoryName(this.asAviFileName.Text);
+            dialog.FileName = System.IO.Path.ChangeExtension(System.IO.Path.GetFileName(this.asAviFileName.Text), "snm");
+
+            if (dialog.ShowDialog(this) == true)
+            {
+                this.msSnmFileName.Text = dialog.FileName;
+            }
+        }
+
+        private void msConvert_Click(object sender, RoutedEventArgs e)
+        {
+            string mf = this.msMfFileName.Text;
+            string snm = this.msSnmFileName.Text;
+
+            if (string.IsNullOrEmpty(mf) || string.IsNullOrEmpty(snm))
+            {
+                return;
+            }
+
+            this.RunBusyAction(disp =>
+            {
+                try
+                {
+                    JeremyAnsel.Xwa.Snm.SnmFile
+                        .FromMFFile(mf)
+                        .Save(snm);
+
+                    disp(() => Xceed.Wpf.Toolkit.MessageBox.Show(this, string.Concat(snm, " created."), this.Title, MessageBoxButton.OK));
+                }
+                catch (Exception ex)
+                {
+                    disp(() => Xceed.Wpf.Toolkit.MessageBox.Show(this, ex.ToString(), this.Title, MessageBoxButton.OK, MessageBoxImage.Error));
+                }
+            });
+        }
+
         private void saOpenSnm_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new OpenFileDialog();
@@ -107,7 +227,7 @@ namespace XwaSnmConverter
                 {
                     JeremyAnsel.Xwa.Snm.SnmFile
                         .FromFile(snm)
-                        .SaveAsAvi(avi);
+                        .SaveAsAviMpeg4(avi);
 
                     disp(() => Xceed.Wpf.Toolkit.MessageBox.Show(this, string.Concat(avi, " created."), this.Title, MessageBoxButton.OK));
                 }
