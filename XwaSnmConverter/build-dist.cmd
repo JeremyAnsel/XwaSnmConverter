6f0ea22b658b9bafa4477f2a68ac199f4766243f
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2022-05-07 14:29:00 +0200

    Switch to VS 2022

--- XwaSnmConverter/build-dist.cmd
+++ XwaSnmConverter/build-dist.cmd
@@ -4,9 +4,9 @@ setlocal
 cd "%~dp0"
 
 For %%a in (
-"XwaSnmConverter\bin\Release\*.dll"
-"XwaSnmConverter\bin\Release\*.exe"
-"XwaSnmConverter\bin\Release\*.config"
+"XwaSnmConverter\bin\Release\net48\*.dll"
+"XwaSnmConverter\bin\Release\net48\*.exe"
+"XwaSnmConverter\bin\Release\net48\*.config"
 ) do (
 xcopy /s /d "%%~a" dist\
 )
--- XwaSnmConverter/dist/_readme_.txt
+++ XwaSnmConverter/dist/_readme_.txt
@@ -1,3 +1,3 @@
 
-XwaSnmConverter runs on Windows 32/64 bits with the .NET framework 4.5.
-(Windows Vista, Windows 7, Windows 8, or superior)
+XwaSnmConverter runs on Windows 32/64 bits with the .NET framework 4.8.
+(Windows 7, Windows 8, Windows 10, Windows 11 or superior)
